3a4116b01b69ec77ef22a61e3bd12804e51fba87
Lê Tiến Sơn <91727716+letienson987@users.noreply.github.com>
2022-09-20 22:36:59 +0700

    Update tests.py

--- tests.py
+++ tests.py
@@ -11,27 +11,15 @@ import requests
 
 
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-driver.get("https://maticstaker.io/?ref=0x324d8b6df22c6cacb49a63d4772397530d7df622&fbclid=IwAR3Gvd2_BZQAYlHNjh2a4qPs3ji_F9o4eyW6Z5tbMaoaKY6RdKanp-C2XYk")
+driver.get("https://seniorautomationtest.com/")
 
+time.sleep(3)
 
+service_button = self.driver.find_element_by_xpath('//a[@href="/services/"]')
+ele.click()
 
-time.sleep(2)
-count = 0
-while (count < 3600):
-    Total_Contract = driver.find_element(By.XPATH,"/html/body/div[1]/div/div[2]/div[1]/div/div[1]/div/div[1]/span/span").text
-
-    if Total_Contract == '0.00 MATIC':
-        count += 1 
-        time.sleep(count)
-    elif Total_Contract != '0.00 MATIC':
-        print(Total_Contract)
-        elm= driver.find_element(By.XPATH,"//h5[contains(text(),'GLOBAL STATS')]")
-        elm.click()
-        time.sleep(2)
-        
-        break
-
-
+time.sleep(3)
+self.assertEqual(//span[@class ='text-capitalize small font-weight-bold'].text, "DỊCH VỤ")
 
 driver.quit()
 
